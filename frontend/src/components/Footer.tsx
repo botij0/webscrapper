088d3d1b8ed5f1a901cf92ed6dc923ca8adaebd3
botij0 <javier.gonzalez.soldado@gmail.com>
2025-03-21 13:13:53 +0100

    feat: header + footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-emerald-700 w-full py-4 text-center">
+            <p className='text-center text-sm text-gray-300'>
+                Built with ❤️ by <a href='https://github.com/botij0' className='text-blue-500'>botij0</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='bg-emerald-700 w-full py-4 text-white text-center'>
+            <h1 className='p-4 md:p-0 md:text-center md:text-3xl font-bold'>Airbnb Listings</h1>
+        </header>
+    )
+}
+
+export default Header
